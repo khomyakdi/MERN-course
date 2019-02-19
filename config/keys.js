a480e2182bc2780085ed86ddbac8fbb1a3d248e5
jobkhomyakdi <job.khomyakdi@gmail.com>
2019-02-19 18:03:32 +0200

    s3l12

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb://brad:abc123!@ds131765.mlab.com:31765/coursedb'
-}
+	mongoURI: 'mongodb://brad:abc123!@ds131765.mlab.com:31765/coursedb',
+	// mongoURI: 'mongodb://localhost/mern',
+	secretOrKey: 'secret'
+};
