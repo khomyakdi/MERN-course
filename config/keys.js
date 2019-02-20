4a8fa196ca9bec450f64201f0367362c01f82401
jobkhomyakdi <job.khomyakdi@gmail.com>
2019-02-20 11:17:45 +0200

    s3l13

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-	mongoURI: 'mongodb://brad:abc123!@ds131765.mlab.com:31765/coursedb',
-	// mongoURI: 'mongodb://localhost/mern',
+	// mongoURI: 'mongodb://brad:abc123!@ds131765.mlab.com:31765/coursedb',
+	mongoURI: 'mongodb://localhost/mern',
 	secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(error => console.log(error));
+	.connect(db)
+	.then(() => console.log('MongoDB Connected'))
+	.catch(error => console.log(error));
 
-app.get('/', (req,res) => res.send('Hello'));
+//Pasport middleware
+app.use(passport.initialize());
+
+//Passport config
+require('./config/passport.js')(passport);
 
 //User Routes
 app.use('/api/users', users);
@@ -26,4 +35,3 @@ app.use('/api/posts', posts);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Serer running on port ${PORT}`));
- 
