f0c7446f262170690c047d0b3c2f075e5ead32af
jobkhomyakdi <job.khomyakdi@gmail.com>
2019-03-18 13:50:14 +0200

    s11l63

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -32,6 +32,18 @@ export const getProfiles = () => dispatch => {
 			dispatch({ type: GET_PROFILE, payload: {} });
 		});
 };
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+	dispatch(setProfileLoading());
+	axios
+		.get(`/api/profile/handle/${handle}`)
+		.then(res => {
+			dispatch({ type: GET_PROFILE, payload: res.data });
+		})
+		.catch(err => {
+			dispatch({ type: GET_PROFILE, payload: null });
+		});
+};
 
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
